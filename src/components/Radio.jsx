6efd544133af28ac5f6555d4e010d7dc87a769d4
Mr-Johny-Blaska <121683366+jan-blaska@users.noreply.github.com>
2023-09-14 00:01:22 +0200

    some comments added to code

--- src/components/Radio.jsx
+++ src/components/Radio.jsx
@@ -1,16 +1,19 @@
 export default function Radio(props) {
     
     function backgroundColor() {
+        // user has selected right answer
         if ((props.selectedAnswer === props.option) && (props.selectedAnswer === props.correctAnswer)) {
             return {
                 backgroundColor: "#94D7A2",
                 border: "1px solid #94D7A2"
             }
+        // user has not selected right answer and this is his wrong answer
         } else if (props.selectedAnswer === props.option) {
             return {
                 backgroundColor: "#F8BCBC",
                 border: "1px solid #F8BCBC"
             }
+        // user has not selected right answer and this is right answer
         } else if (props.correctAnswer === props.option) {
             return {
                 backgroundColor: "#94D7A2",
@@ -19,6 +22,7 @@ export default function Radio(props) {
         }
     }
     
+    // in the case, the form has been submitted, radio options are styled
     const styles = props.formSubmit 
         ? 
         backgroundColor()
